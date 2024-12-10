a25d0fb89ed669fcd7c0cfbba4c00d4d3827954f
laurynasvil <laurynasvileita@gmail.com>
2024-12-10 13:29:08 +0200

    judejimas

--- scripts.js
+++ scripts.js
@@ -1,38 +1,60 @@
-window.addEventListener("load", function(){
-    let scoreBlock = this.document.querySelector(".main-game .score");
+window.addEventListener("load", function () {
+    let scoreBlock = document.querySelector(".main-game .score");
+    let clickZone = document.querySelector(".main-game .click-zone");
     let score = 0;
     let ownedFactories = [];
 
-    document.querySelector(".main-game .click-zone").onclick = function() {
+    
+    clickZone.onclick = function () {
         score += 10;
         scoreBlock.innerText = score;
+    };
+
+    
+    function moveClickZone() {
+        const gameArea = document.querySelector(".main-game");
+        const maxWidth = gameArea.clientWidth - clickZone.offsetWidth;
+        const maxHeight = gameArea.clientHeight - clickZone.offsetHeight;
+
+       
+        const randomX = Math.random() * maxWidth;
+        const randomY = Math.random() * maxHeight;
+
+        clickZone.style.left = randomX + "em";
+        clickZone.style.top = randomY + "em";
     }
 
-    document.querySelectorAll(".factory").forEach(function(fc) {
+    
+    setInterval(moveClickZone, 2000);
+
+    
+    document.querySelectorAll(".factory").forEach(function (fc) {
         let factory = {
             title: fc.querySelector(".title").innerText,
-            costs: parseInt (fc.querySelector(".price").innerText),
+            costs: parseInt(fc.querySelector(".price").innerText),
             isAdding: parseInt(fc.querySelector(".adds").innerText),
-            count: parseInt(fc.querySelector(".count").innerText.slice(1,-1)),
+            count: parseInt(fc.querySelector(".count").innerText.slice(1, -1)),
             button: fc.querySelector("button")
         };
-        factory.button.onclick = function() {
+
+        factory.button.onclick = function () {
             if (score >= factory.costs) {
                 score -= factory.costs;
                 factory.count++;
                 scoreBlock.innerText = score;
                 fc.querySelector('.count').innerText = '(' + factory.count + ')';
-
             }
-        }
-        ownedFactories.push(factory)
+        };
+        ownedFactories.push(factory);
     });
-    console.log(ownedFactories);
 
-    setInterval(function(){
-       score += ownedFactories
-       .map(x => x.count * x.isAdding)
-       .reduce((partial_sum, number) => partial_sum + number, 0);
+    setInterval(function () {
+        score += ownedFactories
+            .map(x => x.count * x.isAdding)
+            .reduce((partial_sum, number) => partial_sum + number, 0);
         scoreBlock.innerText = score;
     }, 1000);
-})
+
+    
+    moveClickZone();
+});
